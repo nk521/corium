d89aaca0f58230ad8535d738fe1380495dab29be
nk521 <nk_mason@protonmail.com>
2020-04-20 01:35:01 +0530

    Adding tests. All the commented stuff has very long outputs. You can try using pprint.

--- apk_parse/main/test.py
+++ apk_parse/main/test.py
@@ -2,41 +2,73 @@
 # -*- coding: utf-8 -*-
 
 from apk import APK
-
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 def test():
     apk_path = "../test.apk"
     #apk_path = "/home/sym/Downloads/share/11/davm/app/com.tmob.AveaOIM.apk"
     apkf = APK(apk_path)
-    # print(f"{apkf.cert_text=}")
-    print(f"{apkf.file_md5=}")
-    print(f"{apkf.cert_md5=}")
-    print(f"{apkf.file_size=}")
-    print(f"{apkf.androidversion=}")
-    print(f"{apkf.package=}")
-    print(f"{apkf.get_android_manifest_xml()=}")
-    print(f"{apkf.get_android_manifest_axml()=}")
-    print(f"{apkf.is_valid_APK()=}")
-    print(f"{apkf.get_filename()=}")
-    print(f"{apkf.get_package()=}")
-    print(f"{apkf.get_androidversion_code()=}")
-    print(f"{apkf.get_androidversion_name()=}")
-    print(f"{apkf.get_max_sdk_version()=}")
-    print(f"{apkf.get_min_sdk_version()=}")
-    print(f"{apkf.get_target_sdk_version()=}")
-    print(f"{apkf.get_libraries()=}")
-    print(f"{apkf.get_files()=}")
-    # pip install python-magic
-    print(apkf.get_files_types())
-    print(apkf.get_dex())
-    print(apkf.get_main_activity())
-    print(apkf.get_activities())
-    print(apkf.get_services())
-    print(apkf.get_receivers())
-    print(apkf.get_providers())
-    print(apkf.get_permissions())
-    print(apkf.show())
-    # apkf.parse_icon(icon_path='/tmp')
+    
+    print(f"{apkf.get_app_name()= }\n")
+    print(f"{apkf.androidversion= }\n")
+    print(f"{apkf.filename= }\n")
+    print(f"{apkf.get_activities()= }\n")
+    print(f"{apkf.get_androidversion_code()= }\n")
+    print(f"{apkf.get_androidversion_name()= }\n")
+    print(f"{apkf.get_declared_permissions()= }\n")
+    print(f"{apkf.get_declared_permissions_details()= }\n")
+    print(f"{apkf.get_details_permissions()= }\n")
+    print(f"{apkf.get_effective_target_sdk_version()= }\n")
+    print(f"{apkf.get_libraries()= }\n")
+    print(f"{apkf.get_main_activities()= }\n")
+    print(f"{apkf.get_main_activity()= }\n")
+    print(f"{apkf.get_max_sdk_version()= }\n")
+    print(f"{apkf.get_min_sdk_version()= }\n")
+    print(f"{apkf.get_package()= }\n")
+    print(f"{apkf.get_providers()= }\n")
+    print(f"{apkf.get_receivers()= }\n")
+    print(f"{apkf.get_requested_aosp_permissions()= }\n")
+    print(f"{apkf.get_requested_aosp_permissions_details()= }\n")
+    print(f"{apkf.get_requested_permissions()= }\n")
+    print(f"{apkf.get_requested_third_party_permissions()= }\n")
+    print(f"{apkf.get_services()= }\n")
+    print(f"{apkf.get_uses_implied_permission_list()= }\n")
+    print(f"{apkf.is_signed()= }\n")
+    print(f"{apkf.is_valid_APK()= }\n")
+    print(f"{apkf.is_wearable()= }\n")
+    # print(f"{apkf.package= }\n")
+    # print(f"{apkf.permissions= }\n")
+    # print(f"{apkf.show= }\n")
+    # print(f"{apkf.uses_permissions= }\n")
+    # print(f"{apkf.valid_apk= }\n")
+
+
+
+    # print(f"{apkf.is_signed_v1= }\n")
+    # print(f"{apkf.is_signed_v2= }\n")
+    # print(f"{apkf.is_signed_v3= }\n")
+    # print(f"{apkf.is_tag_matched= }\n")
+
+    ## Really long outputs
+    # print(f"{apkf.files=}")
+    # print(f"{apkf.get_raw()=}") # throws the entire ZIP
+    # print(f"{apkf.get_certificates()=}")
+    # print(f"{apkf.get_certificates_der_v2()=}")
+    # print(f"{apkf.parse_signatures_or_digests=}")
+    # print(f"{apkf.parse_v2_signing_block=}")
+    # print(f"{apkf.get_signature()=}")
+    # print(f"{apkf.get_signature_name()=}")
+    # print(f"{apkf.get_signature_names()=}")
+    # print(f"{apkf.get_signatures()=}")
+    # print(f"{apkf.parse_v2_v3_signature=}")
+    # print(f"{apkf.parse_v3_signing_block=}")
+    # print(f"{apkf.read_uint32_le=}")
+    # print(f"{apkf.permission_module=}")
+    # print(f"{apkf.get_certificates_der_v3()=}")
+    # print(f"{apkf.get_certificates_v1()=}")
+    # print(f"{apkf.get_certificates_v2()=}")
+    # print(f"{apkf.get_certificates_v3()=}")
 
 if __name__ == "__main__":
     test()
