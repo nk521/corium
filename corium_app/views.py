62b241a97043868c36fa725968f0dfe9322d2dbe
nk521 <nk_mason@protonmail.com>
2020-04-26 22:09:22 +0530

    Add jobs.toml file 1. jobs will be added in jobs.toml and then a cron will update the output toml 2. design edits in static index.html 3. added some jinja templating in index.html 4. added home function in views

--- corium_app/views.py
+++ corium_app/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-	context = {}
+	
+	# apps -> "name" : [ "page/slug link"
+	#					 "current version",
+	#					 "count total of permissions",
+	#					 "count of harmful permissions",
+	#					 "Rating"]
+
+	apps = {
+		"Arogya Setu": ["#", "4.4.2", "4", "3", "1"],
+		"Mahakavach": ["#", "1.6.5", "16", "8", "2"]
+	}
+	context = {"apps": apps}
 	return render(request, 'corium_app/index.html', context)
